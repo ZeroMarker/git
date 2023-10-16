20e81b9288533ddebfa08f67174cff355723ce8d
ZeroMarker <u78k4l67@163.com>
2023-10-16 18:38:03 +0800

    Add show difference local remote

--- exercise.sh
+++ exercise.sh
@@ -4,3 +4,7 @@ git add -A
 git commit -m 'Added my project'
 git remote add origin git@github.com:sammy/my-new-project.git
 git push -u -f origin main
+
+Show difference between local and remote branches
+git fetch origin
+git diff --name-only main origin/main
